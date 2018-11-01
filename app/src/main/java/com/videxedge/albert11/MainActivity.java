73a5dbb7593bd0e4b2992f85a612c73ba499c1fa
albertschool2015 <albert.school2015@gmail.com>
2018-11-01 09:06:35 +0200

    End of programming

--- app/src/main/java/com/videxedge/albert11/MainActivity.java
+++ app/src/main/java/com/videxedge/albert11/MainActivity.java
@@ -2,12 +2,22 @@ package com.videxedge.albert11;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.btn);
+    }
+
+    public void btnclick(View view) {
+        btn.setText("Oh Yea, I've been clicked");
     }
 }
